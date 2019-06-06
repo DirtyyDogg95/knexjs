04ba4481c760c4b72289ca39c43d95c1ef005929
DirtyyDogg95 <48395214+DirtyyDogg95@users.noreply.github.com>
2019-06-06 07:31:20 +0200

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,59 @@
+// Update with your config settings.
+// module.exports = {
+//development: {
+//  client: 'sqlite3',
+//  connection: {
+//    filename: './dev.sqlite3'
+//  }
+//},
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/test_db',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/development_db',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+
+};
