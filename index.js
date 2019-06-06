0b002de20d9ef0d9f0289866b0a5c4d1efd34edf
Kanker Hoer <rbreddels@mauwmauw.eu>
2019-06-06 09:51:09 +0200

    Setup knex

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const store = require('./store')
+
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
--- knexfile.js
+++ knexfile.js
@@ -1,59 +1,14 @@
-// Update with your config settings.
-// module.exports = {
-//development: {
-//  client: 'sqlite3',
-//  connection: {
-//    filename: './dev.sqlite3'
-//  }
-//},
 module.exports = {
-  test: {
-    client: 'pg',
-    connection: 'postgres://localhost/test_db',
-    migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds/test'
-    }
+  client: 'mysql',
+  connection: {
+    user: 'rbreddels',
+    password: 'Habbo!995',
+    database: 'porno'
   },
-
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/development_db',
-    migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds/development'
-    }
+  migrations: {
+    directory: __dirname + '/db/migrations'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds/production'
-    }
+  seeds: {
+    directory: __dirname + '/db/seeds/test'
   }
-
-};
+}
